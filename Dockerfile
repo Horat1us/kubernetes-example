bf3a5f5c321b741e80d930bb3af0301f4a422557
Alexander Letnikow <reclamme@gmail.com>
2017-11-14 16:07:03 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM ubuntu:xenial
+LABEL maintainer="Alexander <horat1us> Letnikow"
+
+WORKDIR /usr/src/app
+
+RUN apt update
+RUN apt -y install nginx php-fpm php-cli php-mbstring git unzip php-zip curl
+
+RUN curl -sS https://getcomposer.org/installer -o composer-setup.php
+RUN php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+
+EXPOSE 80
+
+COPY composer.json .
+COPY composer.lock .
+
+RUN composer install
+
+COPY . /usr/src/app
+
+COPY nginx/production.nginx /etc/nginx/sites-enabled/default
+
+CMD service php7.0-fpm start && nginx -g daemon off;
---
+++ src/index.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php');
+
+$username = getenv("DB_USERNAME");
+$password = getenv("DB_PASSWORD");
+$hostname = getenv("DB_HOSTNAME");
+$port = getenv("DB_PORT");
+
+$database = getenv("DB_NAME");
+
+$connection = new \PDO("pgsql:dbname={$database};host=$hostname;port=$port", $username, $password);
+$postgres = $connection->query("SELECT version();", PDO::FETCH_ASSOC)->fetchColumn();
+
+$info = [
+    "status" => "online",
+    "postgres" => $postgres,
+    "php" => PHP_VERSION,
+    "system" => php_uname(),
+    "faker" => \Faker\Factory::create()->name,
+];
+
+header("Content-Type", "application/json");
+echo json_encode($info);
